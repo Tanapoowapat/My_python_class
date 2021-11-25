1b5f077dc097afd634db195c3cacb860af22b294
Tanapoowapat Yomsarn <tanapoowapat@gmail.com>
2021-11-25 23:25:56 +0700

    game complete

--- pygame/tetris.py
+++ pygame/tetris.py
@@ -144,39 +144,33 @@ T = [['.....',
 
 # นำตัวแปรรูปร่างด้านบนมา นำมาใส่ใน list
 shapes = [S, Z, I, O, J, L, T]
-# เก็บค่าสีไว้ใน list
+# เก็บค่าสีไว้ใน list   S          Z          I       O        J        L       T
 shape_colors = [(salmon), (tomato), (yellow), (lime), (purple), (blue), (pink)]
 
 
 class Piece(object):
     def __init__(self, x, y, shape):
-        self.x = x
-        self.y = y
+        self.x = x # เเนวนอน
+        self.y = y # แนวตั้ง
         self.shape = shape
         self.color = shape_colors[shapes.index(shape)] # นำ list รูปร่างมาใส่สีตาม ตำแหน่ง
-        self.rotation = 0
+        self.rotation = 0 # การหมุนรูปร่าง
 
+
+
+
+#
 def convert_shape(shape):
-
-    #
-    #
-    # หาเลข 0
-
-
     positions = []
     format = shape.shape[shape.rotation % len(shape.shape)]
-
     for i, line in enumerate(format):
-        row = list(line) # ['.','.','0','.','.']
-        for j, column in enumerate(row):
+        row = list(line) #sample data ['..0..']
+        for j, column in enumerate(row):  # sample data ['.','.','0','.','.']
             if column == '0': # หาเลข 0
-                positions.append((shape.x + j, shape.y + i))
-
-
-    # ขยับรูปร่าง
+                positions.append((shape.x + j, shape.y + i)) #เพิ่มเข้า list pos
+    # จัดดูปร่างให้อยู่ตรงกลาง
     for i, pos in enumerate(positions):
         positions[i] = (pos[0] - 2, pos[1] - 4)
-
     return positions
 
 
@@ -186,9 +180,7 @@ def get_shape():
 
 
 def next_shape(shape, game_window):
-
     # เหมือน covert shape แต่จะวาวดรูปร่างแทน
-
     font = pygame.font.SysFont('itim', 30)
     next_shape_label = font.render('Next Shape', 1,(white))
     word_x = x_margin + play_width + 50
@@ -203,18 +195,20 @@ def next_shape(shape, game_window):
 
     game_window.blit(next_shape_label, (word_x + 10, word_y - 30))
 
+
+
+
+
 # เข็คว่าตำแหน่งรูปร่างที่จะตกไปนั้นว่างหรือไม่
 # ส่งค่า grid และ shape
 # สร้าง List ขึ้นมาและเเช็คว่า ตำแหน่งนั้นมีสีหรือไม่
-#
-
 def empty_space(shape, grid):
     accept_pos = [[(j, i) for j in range(10) if grid[i][j] == (0, 0, 0)] for i in range(20)]
     accept_pos = [j for sub in accept_pos for j in sub]
     format = convert_shape(shape)
     for pos in format:
         if pos not in accept_pos:
-            if pos[1] > -1:
+            if pos[1] > -1:             #check ไม่ให้ไม่ด้านข้าง
                 return False
     return True
 
@@ -222,7 +216,7 @@ class play_zone():
 
     # สร้างตาราง x = 10 , y 20 และเก็บค่าสีเเข้าใน list
     # lock_position เป็น Dictionary ใช้เก็บค่าสีโดยอ้างอิง จากตำแหน่งใน Grid
-    # sample data in Dictionary = ((x,y):(tomato))
+    # sample data in lock position = ((x,y):(tomato))
     def create_grid(lock_position={}):
         grid = [[(0,0,0) for __ in range(10)]for __ in range(20)]
         for x in range(len(grid)):
@@ -247,55 +241,43 @@ class play_zone():
 
     #วาดพื้นที่เล่น
     def draw_window(game_window, grid, score=0, last_score=0, level=0):
-
         game_window.blit(bg_main_game, (0, 0))
         font = pygame.font.SysFont('impact', 60)
-
-
         #วาดพื้นหลังสีเทา
         pygame.draw.rect(game_window, (128,128,128), pygame.Rect(x_margin-50, top_margin-60, play_width + 50, play_height + 30))
         #วาดกรอบสีดำครอบพื้นหลัง
         pygame.draw.rect(game_window, (0,0,0),
                          pygame.Rect(x_margin - 50, top_margin - 60, play_width + 50, play_height + 30),5)
-
         label = font.render('Tertis', True, (255, 0, 0))
         label_x = x_margin + play_width - 250
         label_y = top_margin + play_height - 620
         game_window.blit(label,( label_x , label_y))
-
         # current score
         font = pygame.font.SysFont('impact', 24)
         score_label = font.render('Score: ' + str(score), 1, (255, 255, 255))
         score_label_x = x_margin + play_width + 50
         score_label_y = top_margin + play_height / 2 - 150
         game_window.blit(score_label, (score_label_x + 5, score_label_y + 70 ))
-
         # last score
         last_score_label = font.render('High Score: ' + str(last_score), 1, (255, 255, 255))
         last_score_label_x = x_margin + play_width + 50
         last_score_label_y = top_margin + play_height/ 2 - 150
         game_window.blit(last_score_label, (last_score_label_x + 5, last_score_label_y + 100))
-
         # Level
         level_label = font.render('Current level : ' + str(level), 5, (255, 255, 255))
         level_label_x = x_margin + play_width + 50
         level_label_y = top_margin + play_height/ 2 - 150
         game_window.blit(level_label, (level_label_x + 5, level_label_y + 130))
-
         # วาดพื้นที่เล่น
         for x in range(len(grid)):
             for y in range(len(grid[x])):
                 pygame.draw.rect(game_window, grid[x][y],
                                  (x_margin + y * box_size, top_margin + x * box_size, box_size, box_size), 0)
-
-
         play_zone.draw_grid(game_window, grid)
         #วาดกรอบพื้นที่เล่น
         pygame.draw.rect(game_window, (red), (x_margin, top_margin, play_width-50, play_height - 50), 5)
 
-
     # เช็คตำแหน่ง (x,y) ใน Dictionary ว่า y น้อยกว่า 1 หรือไม่
-
     def check_conner(lock_pos):
         for pos in lock_pos:
             x, y = pos
@@ -306,18 +288,18 @@ class play_zone():
 
 
     # เช็คแถวว่าเต็มไหมหลักการเช็คคือจะเช็คสีใน grid หากมีสีอื่นที่ไม่ใช่สีดำจะลบค่า Lock_position
-
+    # เช็คจากข้างล่างขึ้นข้างบน
+    # เมื่อลบแถวแล้วจะต้องเพิ่มแถวใหม่เข้าไป
     def check_tetris(grid, lock_pos):
-        index = 0
-        increases = 0
-        for i in range(len(grid) - 1, -1, -1):
+        index = 0 # Pointer
+        increases = 0 #จำนวนแถวที่นำออก
+        for i in range(len(grid) - 1, -1, -1): #check row จากหลังมาด้านหน้า
             row = grid[i]
             if (0, 0, 0) not in row:
                 increases += 1
                 index = i
                 for j in range(len(row)):
                     del lock_pos[(j, i)]
-
         if increases > 0:
             for key in sorted(list(lock_pos), key = lambda x: x[1])[::-1]:
                 # sample data [(0,1),(0,0)]
@@ -327,7 +309,6 @@ class play_zone():
                     newKey = (x, y + increases)
                     mixer.Sound.play(clear_row_sound)
                     lock_pos[newKey] = lock_pos.pop(key)
-
         return increases
 
 #score
@@ -348,36 +329,29 @@ def get_high_score():
         return high_score
 
 def main_game():
-
     run = True
-    lock_position = {}
-
+    lock_position = {} #
     change_piece = False #เช็คว่าต้องเปลี่ยนรูปร่างไหม
     current_piece = get_shape() #รูปร่างปัจจุบัน
     next_piece = get_shape() #รูปร่างต่อไปที่จะตก
-
     clock = pygame.time.Clock()
     fall_time = 0   #เก็บค่า loop ว่ารันไปแล้วกี่ครั้ง
     fall_speed = 0.30 #ความเร็วของรูปร่างที่จะตกลงมา
-
     #set up score
-    level = 0
+    level = 4
     high_score = get_high_score()
-    score = 0
-
-    #mixer.music.play(-1)
-
+    score = 4800
+    mixer.music.play(-1)
+    mixer.music.set_volume(0.01)
     #game running
+
     while run:
-
-
         grid = play_zone.create_grid(lock_position)
-        fall_time += clock.get_rawtime() #จะเช็คว่า while lopp ทำงานนานเท่าไหร่
+        fall_time += clock.get_rawtime() #จะเช็คว่า while loop ทำงานนานเท่าไหร่
         clock.tick()
 
         #game control
         pressed = lambda key: event.type == pygame.KEYDOWN and event.key == key #สร้าง function มาเก็บค่าเวลากด คีบอร์ดไว้
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -404,7 +378,6 @@ def main_game():
                 if not (empty_space(current_piece, grid)):
                     current_piece.rotation -= 1
 
-
         #incress level
         if score >= 1000 and score < 2000:
             level = 1
@@ -426,10 +399,9 @@ def main_game():
         if fall_time / 1000 >= fall_speed:
                 fall_time = 0
                 current_piece.y += 1
-                if not (empty_space(current_piece, grid)) and current_piece.y > 0:
+                if not (empty_space(current_piece, grid)) and current_piece.y > 0: #เช็คว่ารูปร่างที่ตกลงไปนั้นเป็น empty space รึป่าว
                     current_piece.y -= 1
                     change_piece = True
-
 
         # get shape
         shape_pos = convert_shape(current_piece)
@@ -445,7 +417,8 @@ def main_game():
         if change_piece:
             for pos in shape_pos:
                 p = (pos[0], pos[1])
-                lock_position[p] = current_piece.color
+                lock_position[p] = current_piece.color #เปลี่ยนสีของ Grid
+                # sample data {(0,2):(lime)}
             current_piece = next_piece
             next_piece = get_shape()
             change_piece = False
@@ -463,7 +436,6 @@ def main_game():
             pygame.display.update()
 
 def main_menu():
-
     game_window.blit(bg,(0,0))
     wood_sign = pygame.image.load('assert/wood.png')
     wood_sign = pygame.transform.scale(wood_sign,(700,350))
@@ -517,29 +489,22 @@ def game_over():
     run = True
     while run:
 
-
-
         font = pygame.font.SysFont("itim", 48)
-
         #game over label
         Game_Over_label = font.render('Game Over!', True , (0,0,0))
         Game_Over_label_x = x_margin + play_width - 240
         Game_Over_label_y = top_margin + play_height - 400
         game_window.blit(Game_Over_label, (Game_Over_label_x, Game_Over_label_y))
-
         #ask play again
         play_again_label = font.render('Press any key to play again!', True, (0,0,0))
         play_again_x = x_margin + play_width - 350
         play_again_y = top_margin + play_height - 350
         game_window.blit(play_again_label, (play_again_x , play_again_y ))
-
-
         #show high score
         high_score_label = font.render('High score : '+str(high_score), True, (0, 0, 0))
         high_score_label_x = x_margin + play_width - 270
         high_score_label_y = top_margin + play_height - 300
         game_window.blit(high_score_label, (high_score_label_x, high_score_label_y))
-
 
         pygame.display.update()
         pygame.time.delay(60)
